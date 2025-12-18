52e4808f560c93a445454099b562061148e56327
Masud <wubetumasud@gmail.com>
2025-12-18 23:47:52 +0300

    Useparams for dynamic parameters

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import classes from './Results.module.css'
 import LayOut from '../../Components/LayOut/LayOut'
+import { useParams } from 'react-router-dom'
 
 
 function Results() {
+  const { categoryName } = useParams();
   return (
     <LayOut>
       <div>
--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,7 @@ import SignUp from './Pages/Auth/SignUp'
 import Payment from './Pages/Payment/Payment'
 import Orders from './Pages/Orders/Orders'
 import Cart from './Pages/Cart/Cart'
+import Category from './Components/Category/Category'
 
 function Routering() {
   return (
@@ -14,6 +15,7 @@ function Routering() {
         <Route path="/auth"  element={<SignUp/>}></Route>
         <Route path="/payments"  element={<Payment/>}></Route>
         <Route path="/orders"  element={<Orders/>}></Route>
+        <Route path="/category/:categoryName"  element={<Results/>}></Route>
         <Route path="/cart"  element={<Cart/>}></Route>
     </Routes>
    </Router>
