47be87cb1929845325ac545bc59c1b885c2b87d4
Masud <wubetumasud@gmail.com>
2025-12-22 00:40:07 +0300

    fix item amount rendered on header cart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,6 +10,9 @@ import { DataContext } from '../DataProvider/DataProvider'
 
 function Header() {
     const [{basket}, dispatch] = useContext(DataContext);
+    const totalItem = basket?.reduce((amount, item) => {
+        return amount + item.amount
+    }, 0)
   return (
     <section className={classes.fixed}>
         <section>
@@ -63,7 +66,7 @@ function Header() {
                     <Link to={"/cart"} className={classes.cart}>
                         {/* icon */}
                         <BiCart size={35} />
-                        <span>{basket.length}</span>
+                        <span>{totalItem}</span>
                     </Link>
                 </div>  
             </div>
