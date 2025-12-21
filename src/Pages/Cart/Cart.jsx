1a581f4e063ef4253dedf03e9534333850a102eb
Masud <wubetumasud@gmail.com>
2025-12-21 22:04:37 +0300

    Add subtotal box

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import classes from './Cart.module.css'
 import LayOut from '../../Components/LayOut/LayOut'
 import { DataContext } from '../../Components/DataProvider/DataProvider'
 import ProductCard from '../../Components/Product/ProductCard'
+import CurrencyFormat from '../../Components/CurrencyFormat/CurrencyFormat'
 
 function Cart() {
   const [{basket, user}, dispatch] = useContext(DataContext);
@@ -20,13 +21,28 @@ function Cart() {
                   key={item.id}
                   product={item}
                   renderDesc={true}
-                  renderAdd={false}
+                  renderAdd={true}
                   flex={true}
                 />
               })
             )
           }
         </div>
+        {
+          basket?.length !== 0 && (
+            <div>
+              <div>
+                <p>Subtotal ({basket?.length} items)</p>
+                <CurrencyFormat amount="total"/>
+              </div>
+              <span>
+                <input type="checkbox"/>
+                <small>This order contains order</small>
+              </span>
+              <Link to='/payments'>Continue to checkout</Link>
+            </div>
+          )
+        }
       </section>
     </LayOut>
   )
