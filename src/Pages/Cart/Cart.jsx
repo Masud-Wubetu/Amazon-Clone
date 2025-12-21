c1111bb517f7249d00b8b66eae668eef7656fa8b
Masud <wubetumasud@gmail.com>
2025-12-21 14:59:37 +0300

    Shwing items in cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import classes from './Cart.module.css'
 import LayOut from '../../Components/LayOut/LayOut'
-
+import { DataContext } from '../../Components/DataProvider/DataProvider'
+import ProductCard from '../../Components/Product/ProductCard'
 
 function Cart() {
+  const [{basket, user}, dispatch] = useContext(DataContext);
   return (
     <LayOut>
-      <div>
-        Cart
-      </div>
+      <section>
+        <div>
+          <h2>Hello</h2>
+          <h3>Your shopping basket</h3>
+          <hr/>
+          {
+            basket?.length === 0 ? (<p>Opps! no item in your cart</p>) : (
+              basket?.map((item) => {
+                 return <ProductCard
+                  key={item.id}
+                  product={item}
+                  renderDesc={true}
+                  flex={true}
+                />
+              })
+            )
+          }
+        </div>
+      </section>
     </LayOut>
   )
 }
