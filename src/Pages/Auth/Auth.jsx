4e41efa6386283082e380860b9d30557b3165b93
Masud <wubetumasud@gmail.com>
2025-12-22 09:16:02 +0300

    Form structure

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -5,10 +5,26 @@ import {Link} from 'react-router-dom'
 function Auth() {
   return (
      <section className={classes.login}>
+      {/* Logo */}
         <Link>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/960px-Amazon_logo.svg.png"
            alt="" />
         </Link>
+        {/* form */}
+        <div>
+          <h1>Sign-in</h1>
+          <div>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <button>
+            Sign In
+          </button>
+        </div>
      </section>
   )
 }
