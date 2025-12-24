5d28b6ff843c450b1bb750e5ae1c07e8fd9e53fe
Masud <wubetumasud@gmail.com>
2025-12-24 14:20:08 +0300

    Dynamically render items in Payment Page

--- Frontend/src/Pages/Payment/Payment.jsx
+++ Frontend/src/Pages/Payment/Payment.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import classes from './Payment.module.css'
 import LayOut from '../../Components/LayOut/LayOut'
+import { DataContext } from '../../Components/DataProvider/DataProvider'
 
 
 function Payment() {
+  const [{basket}] = useContext(DataContext);
+  const totalItem = basket?.reduce((amount, item) => {
+        return amount + item.amount
+    }, 0)
   return (
     <LayOut>
-      <div>
-        Payment
-      </div>
+      {/* header */}
+      <div className={classes.payment_header}>Checkout ({totalItem}) items</div>
+      {/* payment method */}
+      <section>
+        {/* address */}
+        <div></div>
+        <hr />
+
+        {/* product */}
+        <div></div>
+        <hr />
+
+        {/* card form */}
+        <div></div>
+
+      </section>
     </LayOut>
   )
 }
