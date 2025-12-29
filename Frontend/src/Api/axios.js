d3281d8d7beb6fc531310e7409f963ce053cc92e
Masud <wubetumasud@gmail.com>
2025-12-29 11:35:53 +0300

    Pin Node version to 18 for Netlify

--- Frontend/src/Api/axios.js
+++ Frontend/src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-92bf6/us-central1/api"
+    //baseURL: "http://127.0.0.1:5001/clone-92bf6/us-central1/api"
+    baseURL: "https://amazon-clone-770v.onrender.com"
 });
 
 export {axiosInstance}
